713042a401eb99539ebdf6a817019178b39301db
Vjekoslav <vjekoslav.jersic@gmail.com>
2021-02-23 20:27:30 +0100

    General setup of project

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin  = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+let mode = "development";
+
+if (process.env.NODE_Env === "production") {
+  mode = "production";
+}
+
+module.exports = {
+  mode: "development",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin(), 
+      new HtmlWebpackPlugin({
+      template: "./src/index.html"
+  })],
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+};
